f9b1a9aee0f826f04a23992b7d580e6e3463d46e
Beth <bethm.meeker@gmail.com>
2021-08-07 16:26:29 -0600

    Add tests for Home user flow

--- cypress/integration/HomeFlow_spec.js
+++ cypress/integration/HomeFlow_spec.js
@@ -2,10 +2,63 @@ describe('Home screen flow', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/Home')
-  })
+  });
 
   it('Should have url that end with /Home', () => {
-    cy.url().should('include', '/Home');
-  })
+    cy.url().should('include', '/Home')
+  });
 
-})
+  it('Should have a header with the app name GET Un-Bored', () => {
+    cy.get('header').contains('GET Un-Bored')
+  });
+
+  it('Should have two buttons for navigation in the header', () => {
+    cy.get('header')
+      .find('button').should('have.length', 2)
+  });
+
+  it('Should have a header that contains an image of a house to navigate to Home', () => {
+    cy.get('header')
+      .find('img').should('have.length', 1);
+  });
+
+  it('Should have a drop down labeled Category', () => {
+    cy.get('label').contains('Category')
+  });
+
+  it('Should be defaulted to undefined', () => {
+    cy.get('select')
+      .should('have.value', 'undefined')
+  });
+
+  it('Should be able to select a different category', () => {
+    cy.get('select')
+      .select('education')
+      .should('have.value', 'education')
+    
+      .select('recreational')
+      .should('have.value', 'recreational')
+
+      .select('social')
+      .should('have.value', 'social')
+
+      .select('DIY')
+      .should('have.value', 'diy')
+
+      .select('charity')
+      .should('have.value', 'charity')
+
+      .select('cooking')
+      .should('have.value', 'cooking')
+
+      .select('relaxation')
+      .should('have.value', 'relaxation')
+
+      .select('music')
+      .should('have.value', 'music')
+
+      .select('busywork')
+      .should('have.value', 'busywork')
+  });
+
+});
